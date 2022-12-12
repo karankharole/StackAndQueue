709375e2bc505fac2d7cf9a2bdf493551a94e6c4
karankharole <karankharole2000@gmail.com>
2022-12-12 20:02:04 +0530

    Dequeue element from the begining

--- Program.cs
+++ Program.cs
@@ -4,13 +4,14 @@
     {
         static void Main(string[] args)
         {
-           
-
             Queue queue = new Queue();
 
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
+            queue.display();
+            Console.WriteLine("=========================");
+            queue.Dequeue();
             queue.display();
 
 
